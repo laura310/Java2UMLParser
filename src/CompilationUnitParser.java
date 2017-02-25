ed122178426a29fc2ef91d9d29dd2e9ff066e8db
Laura Zhou <laurajoe@Lauras-MacBook-Air.local>
2017-02-25 10:28:42 -0800

    Can parse java src to yUML recoganizable codes. Graph generator to be added.

---
+++ src/CompilationUnitParser.java
@@ -0,0 +1,270 @@
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * Created by laurajoe on 2/20/17.
+ */
+public class CompilationUnitParser {
+
+    CompilationUnit compilationUnit;
+    private String parsedCode;
+    private HashMap<String, Boolean> mapIfInterface;    // to tell if a certain .java is an interface or not
+    private HashMap<String, String> classRelationMap;  // the relationship between different .java files
+
+    private String parsedClassInfo;
+    private String className;
+    private String methods;     // parsed code for methods
+    private String fields;      // parsed code for fields
+    private String relations;   // parsed code for relations
+    ArrayList<String> makeGetterSetterPublicAttri;
+
+
+
+    /**
+     * Constructor
+     */
+    public CompilationUnitParser(CompilationUnit cu, HashMap<String, Boolean> mapIfInterface, HashMap<String, String> classRelationMap) {
+        this.compilationUnit = cu;
+        this.mapIfInterface = mapIfInterface;
+        this.classRelationMap = classRelationMap;
+
+        parsedCode = "";
+        parsedClassInfo = "";
+        className = "";
+        methods = "";
+        fields = "";
+        relations = ",";
+        makeGetterSetterPublicAttri = new ArrayList<>();
+    }
+
+    /***
+     *
+     * @return parsed code of a CompilationUnit
+     */
+    public String parse() {
+
+        // assuming only one class in one .java file
+        ClassOrInterfaceDeclaration coiDecl = (ClassOrInterfaceDeclaration) compilationUnit.getTypes().get(0);
+
+        // parse class info
+        if (coiDecl.isInterface())   parsedClassInfo = "[" + "<<interface>>;";
+        else                         parsedClassInfo = "[";
+        parsedClassInfo += coiDecl.getName();
+        className = coiDecl.getName();
+
+
+        for (BodyDeclaration bd : coiDecl.getMembers()) {
+
+            if (bd instanceof FieldDeclaration) {
+                parseFieldDeclaration(bd);
+            }
+
+            else if (bd instanceof ConstructorDeclaration) {
+                parseConstructorDeclaration(bd, coiDecl);
+            }
+
+            else if (bd instanceof MethodDeclaration) {
+                parseMethodDeclaration(bd, coiDecl);
+            }
+        }
+
+
+        // check if extends from other class
+        if (coiDecl.getExtends() != null) {
+            for(ClassOrInterfaceType coiTmp : coiDecl.getExtends()) {
+                if(coiTmp != null) {
+                    relations += "[" + className + "] " + "-^ " + coiDecl.getExtends().toString();
+                    relations += ",";
+                }
+            }
+        }
+
+        // check implement from some interface
+        if (coiDecl.getImplements() != null) {
+            for (ClassOrInterfaceType intface : coiDecl.getImplements()) {
+                relations += "[" + className + "] " + "-.-^ " + "[" + "<<interface>>;" + intface + "]";
+                relations += ",";
+            }
+        }
+
+        // Combine ParsedClassInfo, methods and fields
+        parsedCode += parsedClassInfo;
+
+        if (!fields.isEmpty()) {
+            parsedCode += "|" + fields;
+        }
+        if (!methods.isEmpty()) {
+            parsedCode += "|" + methods;
+        }
+        parsedCode += "]";
+
+        parsedCode += relations;
+
+        return parsedCode;
+    }
+
+    /**
+     * Parsing Fields, only include private and public attributes.
+     * For a FieldDeclaration, there are: scope, fieldClassName, and variableName.
+     */
+    private void parseFieldDeclaration(BodyDeclaration bd) {
+        FieldDeclaration fd = ((FieldDeclaration) bd);
+
+        String scope = convertAccessModifierToSym(bd.toStringWithoutComments().substring(0, bd.toStringWithoutComments().indexOf(" ")));
+        String fieldClassName = convert2Parenthesis(fd.getChildrenNodes().get(0).toString());
+        String variableName = fd.getChildrenNodes().get(1).toString();
+
+        // i.e. private Collection<Observer> observers = new ArrayList<Observer>() ;
+        if (variableName.contains("="))
+            variableName = variableName.split(" ")[0];
+
+        // if this attribute can be accessed by public getters/setters, it should be public attribute in UML
+        if (scope.equals("-") && makeGetterSetterPublicAttri.contains(variableName.toLowerCase()))
+            scope = "+";
+
+        /** populate the classRelationMap **/
+        String relationClass = "";
+        boolean getRelationMultiple = false;
+
+        if (fieldClassName.contains("(")) {
+            getRelationMultiple = true;
+            String potentialRelationClass = fieldClassName.substring(fieldClassName.indexOf("(") + 1, fieldClassName.indexOf(")"));
+            relationClass = mapIfInterface.containsKey(potentialRelationClass) ? potentialRelationClass : relationClass;
+
+        } else if (mapIfInterface.containsKey(fieldClassName))
+            relationClass = fieldClassName;
+
+        if (relationClass.length() > 0 && mapIfInterface.containsKey(relationClass)) {
+            String relation = getRelationMultiple ? "-*" : "-";
+            classRelationMap.put(this.className + "-" + relationClass, relation);
+        }
+
+        if (scope == "+" || scope == "-")
+            fields += scope + " " + variableName + " : " + fieldClassName + "; ";
+    }
+
+
+    /**
+     * Parsing Constructors.
+     * Constructors are treated as public methods in UML graph.
+     */
+    private void parseConstructorDeclaration(BodyDeclaration bd, ClassOrInterfaceDeclaration coiDecl) {
+        ConstructorDeclaration cd = (ConstructorDeclaration) bd;
+
+        if (cd.getDeclarationAsString().startsWith("public") && !coiDecl.isInterface()) {
+
+            methods += "+ " + cd.getName() + "("; // methods prefix
+            for (Object childNode : cd.getChildrenNodes()) {
+                if (childNode instanceof Parameter) {
+                    parseParameterInMethods(childNode);
+                }
+            }
+            methods += ");"; // methods postfix
+        }
+    }
+
+
+    /**
+     * Parsing Methods.
+     * Public Setters/Getters should be interpreted as "Public Attributes".
+     */
+    private void parseMethodDeclaration(BodyDeclaration bd, ClassOrInterfaceDeclaration coiDecl) {
+
+        MethodDeclaration md = ((MethodDeclaration) bd);
+
+        // Get only public methods
+        if (md.getDeclarationAsString().startsWith("public") && !coiDecl.isInterface()) {
+
+            // Public Setters/Getters should be interpreted as "Public Attributes" , test-case-3
+            if (md.getName().startsWith("get") || md.getName().startsWith("set")) {
+                String varName = md.getName().substring(3);
+                makeGetterSetterPublicAttri.add(varName.toLowerCase());
+
+            } else {
+
+                methods += "+ " + md.getName() + "(";  // methods prefix
+                for (Object childNode : md.getChildrenNodes()) {
+                    if (childNode instanceof Parameter) {
+                        parseParameterInMethods(childNode);
+
+                    } else {
+                        String methodBody[] = childNode.toString().split(" ");
+                        for (String foo : methodBody) {
+                            if (mapIfInterface.containsKey(foo)
+                                    && !mapIfInterface.get(className)) {
+                                relations += "[" + className
+                                        + "] uses -.->";
+                                if (mapIfInterface.get(foo))
+                                    relations += "[<<interface>>;" + foo
+                                            + "]";
+                                else
+                                    relations += "[" + foo + "]";
+                                relations += ",";
+                            }
+                        }
+                    }
+                }
+                methods += ") : " + md.getType() + ";";
+            }
+        }
+    }
+
+    private void parseParameterInMethods(Object childNode) {
+        String paramClass = ((Parameter) childNode).getChildrenNodes().get(1).toString();
+        String paramName = ((Parameter) childNode).getChildrenNodes().get(0).toString();
+        methods += paramName + " : " + paramClass;
+
+        if (mapIfInterface.containsKey(paramClass)) {
+            relations += "[" + className + "] uses -.->";
+            if (mapIfInterface.get(paramClass))
+                relations += "[<<interface>>;" + paramClass + "]";
+            else
+                relations += "[" + paramClass + "]";
+        }
+        relations += ",";
+    }
+
+    /**
+     * Avoid confusing with yUML language of "[", "]", "<<", and ">>".
+     *
+     * replace "[" and "<" in a String with "("
+     * replace "]" and ">" in a String with ")"
+     *
+     * @param string
+     * @return processed string
+     */
+    private String convert2Parenthesis(String string) {
+        string = string.replace("[", "(");
+        string = string.replace("]", ")");
+        string = string.replace("<", "(");
+        string = string.replace(">", ")");
+
+        return string;
+    }
+
+
+    /**
+     * In UML: public: "+"; private: "-"; protected: "#".
+     *
+     * @param accessModifier
+     * @return
+     */
+    private String convertAccessModifierToSym(String accessModifier) {
+        switch (accessModifier) {
+            case "private":
+                return "-";
+            case "public":
+                return "+";
+            case "protected":
+                return "#";
+            default:
+                return "";
+        }
+    }
+
+}
--- src/SrcParser.java
+++ src/SrcParser.java
@@ -2,31 +2,136 @@
  * Created by laurajoe on 2/19/17.
  */
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import com.github.javaparser.ast.*;
-
+import java.io.FileInputStream;
+import com.github.javaparser.*;
+import com.github.javaparser.ast.body.*;
+import java.util.*;
 
 public class SrcParser {
 
-    String folderName;
-//    String umlGraph;
-    ArrayList<CompilationUnit> compilationUnits = new ArrayList<CompilationUnit>();
+    private String parsedCode;                          // code that can be recognized by yUML
+    private String folderName;                          // provided by user as parameter
+//    private String umlGraph;                      //provided by user as parameter
+    private HashMap<String, Boolean> mapIfInterface;    // to tell if a certain .java is an interface or not
+    private HashMap<String, String> classRelationMap;  // the relationship between different .java files
+    private ArrayList<CompilationUnit> compilationUnits;     // one .java file is one CompilationUnit
 
+
+    /**
+     * Constructor
+     * @param folderName
+     */
     public SrcParser(String folderName) {
         this.folderName = folderName;
 //        this.umlGraph = umlGraphName;
+        mapIfInterface = new HashMap<>();
+        parsedCode = "";
+        classRelationMap = new HashMap<>();
+        compilationUnits = new ArrayList<>();
     }
+
 
     public void run() {
-
+        // to get the project root path
         String projRootPath = new File("f").getAbsolutePath();
         projRootPath = projRootPath.substring(0, projRootPath.length()-2); //to get rid of the ending "/f"
-//        System.out.println("url = " + projRootPath);  //DEBUG
 
 
-        File folder = new File(projRootPath + "/" + folderName);
-        for(File f : folder.listFiles()) {
-            System.out.println(f.getName());
+        getCompilationUnits(projRootPath + "/" + folderName);
+        checkIfInterface(compilationUnits);
+
+        for (CompilationUnit cu : compilationUnits) {
+            CompilationUnitParser cuParser = new CompilationUnitParser(cu, mapIfInterface, classRelationMap);
+            parsedCode += cuParser.parse();
+        }
+
+        parsedCode += addClassRelations();
+
+        System.out.println("Parsed Code: " + parsedCode); // FOR DEBUG.
+
+        // UMLGenerator
+    }
+
+
+
+    /***
+     * Return a list of CompilationUnits in the given file path.
+     *
+     * @param filePath
+     */
+    private void getCompilationUnits(String filePath) {
+        File file = new File(filePath);
+
+        for(File f : file.listFiles()) {
+            if(f.isFile() && f.getName().endsWith(".java")) {
+                FileInputStream in = null;
+                try {
+
+                    in = new FileInputStream(f);
+                    CompilationUnit cu = JavaParser.parse(in);
+                    compilationUnits.add(cu);
+
+                } catch (FileNotFoundException e) {
+                    System.out.println("FileNotFoundException from getCompilationUnits method");
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    System.out.println("Exception from getCompilationUnits method");
+                    e.printStackTrace();
+                } finally {
+                    try {
+
+                        in.close();
+
+                    } catch (IOException e) {
+                        System.out.println("IOException from getCompilationUnits method");
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
+
+
+    /***
+     * For every .java file, check if it is an Interface or not.
+     *
+     * @param compilationUnits
+     */
+    private void checkIfInterface(ArrayList<CompilationUnit> compilationUnits) {
+        for(CompilationUnit cu : compilationUnits) {
+            List<TypeDeclaration> typeDeclarations = cu.getTypes();
+
+            // actually, we assume there's only one TypeDeclaration in one CompilationUnit
+            for(TypeDeclaration td : typeDeclarations) {
+                mapIfInterface.put(td.getName(), ((ClassOrInterfaceDeclaration) td).isInterface());
+            }
+        }
+    }
+
+
+
+    private String addClassRelations() {
+
+        String result = "";
+        Set<String> keys = classRelationMap.keySet(); // get all keys
+        for (String key : keys) {
+            String[] classes = key.split("-");
+            if (mapIfInterface.get(classes[0]))         result += "[<<interface>>;" + classes[0] + "]";
+            else                                        result += "[" + classes[0] + "]";
+
+            result += classRelationMap.get(key); // Add connection
+
+            if (mapIfInterface.get(classes[1]))         result += "[<<interface>>;" + classes[1] + "]";
+            else                                        result += "[" + classes[1] + "]";
+
+            result += ",";
+        }
+        return result;
+    }
+
+
 }
