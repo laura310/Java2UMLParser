8b3b27daa1192ac70d216de3cd4051b421de7674
Laura Zhou <laurajoe310@gmail.com>
2017-04-30 16:23:54 -0700

    Remove seqParser. Only TWO parameters needed now.

--- src/Java2UML.java
+++ src/Java2UML.java
@@ -3,17 +3,24 @@
  */
 public class Java2UML {
     public static void main(String[] args) {
-        if(args[0].equals("class")) {
+//        if(args[0].equals("class")) {
+//
+//            SrcParserClass srcParserClass = new SrcParserClass(args[1], args[2]);
+//            srcParserClass.run();
+//        } else if(args[0].equals("seq")) {
+//
+//            SrcParserSeq srcParserSeq = new SrcParserSeq(args[1], args[2], args[3], args[4]);
+//            srcParserSeq.run();
+//        } else {
+//
+//            System.out.println("First Parameter should be \"class\" (Class Diagram) or \"seq\" (Sequence Diagram)");
+//        }
 
-            SrcParserClass srcParserClass = new SrcParserClass(args[1], args[2]);
-            srcParserClass.run();
-        } else if(args[0].equals("seq")) {
-
-            SrcParserSeq srcParserSeq = new SrcParserSeq(args[1], args[2], args[3], args[4]);
-            srcParserSeq.run();
-        } else {
-
-            System.out.println("First Parameter should be \"class\" (Class Diagram) or \"seq\" (Sequence Diagram)");
+        if(args.length != 2) {
+            System.out.println("Two parameters: Resource path and output path, are needed.");
         }
+
+        SrcParserClass srcParserClass = new SrcParserClass(args[0], args[1]);
+        srcParserClass.run();
     }
 }
